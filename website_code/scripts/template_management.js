48f34f91b2ca98df51637ed393a2fba600d9b0ce
NSavenije <noud.savenije@hotmail.com>
2019-05-22 14:19:42 +0200

    Cleaned files

--- website_code/scripts/template_management.js
+++ website_code/scripts/template_management.js
@@ -486,6 +486,11 @@ function preview_window(admin) {
 
                 if (node.xot_type == "file") {
 
+                    if (node.type == "langtools")
+                        {
+                            node.editor_size = "1280, 768"
+                            node.preview_size = "802,602"
+                        }
                     size = node.preview_size.split(",");
                     if (size.length != 1) {
 
