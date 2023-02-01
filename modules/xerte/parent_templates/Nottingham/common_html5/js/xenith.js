d74ba8afb687660600d9179b1fd20a5f214dc3c3
Noud Liefrink <noudliefrink@msn.com>
2023-02-01 16:48:56 +0100

    Load annotatedDiagram block in regular annotatedDiagram
    Works, however the css of the block is still messed up which breaks both

--- modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
+++ modules/xerte/parent_templates/Nottingham/common_html5/js/xenith.js
@@ -2622,6 +2622,7 @@ function x_navigateToPage(force, pageInfo, addHistory) { // pageInfo = {type, ID
 
 //INTERN CODE
 function x_createBlock(container, module, modulePosition){
+	debugger;
 	//Create the area for the block to be populated in. Then call the init of the block.
 	var blockid = "block" + modulePosition;
 	var jsName = module.tagName.replace("Block", "")
